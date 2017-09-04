b78c43c792f2b4a24c35d5543010a35bdba99f2b
JungHyun <freenham@gmail.com>
2017-09-04 16:59:04 +0900

    Math클래스의 static 메소드들을 살펴보았다. 너무쉬웠다.

---
+++ javaAdvanced/src/javaAdvanced/MathExam.java
@@ -0,0 +1,21 @@
+package javaAdvanced;
+
+public class MathExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value1 = Math.max(5, 30);
+		System.out.println(value1);
+		
+		int value2=Math.min(5, 30);
+		System.out.println(value2);
+		
+		double value3 = Math.cos(Math.PI);
+		System.out.println(value3);
+		
+		System.out.println(Math.random());
+		System.out.println(Math.sqrt(225));
+		
+	}
+
+}
