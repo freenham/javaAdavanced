cb95a097847d37e9aff4d45b71d07f76dbf5e890
JungHyun <freenham@gmail.com>
2017-09-04 17:15:39 +0900

    Integer클래스의 메소드에 대해 연습해보았다. 귀찮았다.

---
+++ javaAdvanced/src/javaAdvanced/IntegerPractice.java
@@ -0,0 +1,10 @@
+package javaAdvanced;
+
+public class IntegerPractice {
+	public static void main(String[] args) {
+		Integer i1 = new Integer(129);
+		Integer i2 = 4;
+		System.out.println(Integer.bitCount(3));
+		System.out.println(i1.byteValue());
+	}
+}
